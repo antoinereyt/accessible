b8679c797fd2ddd80880894da5a1b1dc57dc5f47
Antoine Reyt <antoine.reyt@zeloce.com>
2020-10-14 20:55:09 +0200

    Fixes warnning for Elixir 1.6

--- lib/accessible.ex
+++ lib/accessible.ex
@@ -5,6 +5,11 @@ defmodule Accessible do
     quote location: :keep do
       @behaviour Access
 
+      if Version.compare(System.version(), "1.7.0") == :lt do
+        defdelegate get(term, key), to: Map
+        defdelegate get(term, key, default), to: Map
+      end
+
       defdelegate fetch(term, key), to: Map
       defdelegate get_and_update(term, key, fun), to: Map
       defdelegate pop(data, key), to: Map
